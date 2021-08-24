9500c25331ccf6bbe1b1d7c1f23dd4e781283c60
LeeSuha-98 <dltngk10@naver.com>
2021-08-24 15:24:25 +0900

    aaa

---
+++ CARESpring/src/main/java/org/care/service/DeleteFoodService.java
@@ -0,0 +1,10 @@
+package org.care.service;
+
+import java.util.List;
+
+import org.care.domain.ReviewInfo;
+
+public interface DeleteFoodService {
+	public void deleteReview(int reviewNo) throws Exception; 
+	List<ReviewInfo> selectReviewDetail(int reviewNo) throws Exception;
+}
